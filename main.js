72ce63a2ab433378ab3f71257ded2ea9a0b3da3c
Pinkify_ <huelleigor@gmail.com>
2025-11-20 20:04:32 +0100

    a

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const upperFold = document.querySelector('.paper-fold:nth-child(1)');
 const lowerFold = document.querySelector('.paper-fold:nth-child(3)');
 
 gsap.set(paper, {
-    scale: .8
+    scale: .8,
+    rotate: 20
 })
 
 gsap.set(upperFold, {
