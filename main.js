c035bd41064e55dc0d99d7e32bee64cc47486d91
Pinkify_ <huelleigor@gmail.com>
2025-11-20 21:06:00 +0100

    test

--- main.js
+++ main.js
@@ -2,14 +2,54 @@ const paper = document.querySelector('.paper');
 const upperFold = document.querySelector('.paper-fold:nth-child(1)');
 const lowerFold = document.querySelector('.paper-fold:nth-child(3)');
 
-const tl = gsap.timeline();
+let canOpen = false
 
-tl.from(paper, {
-    yPercent: 100,
-    scale: .8,
-    rotate: 20
-}).from(upperFold, {
-    rotateX: "179deg" 
-}).from(lowerFold, {
+const openTl = gsap.timeline();
+openTl.pause();
+
+gsap.set([upperFold, lowerFold], {
     rotateX: "179deg"
+})
+
+gsap.set(paper, {
+    rotate: 10,
+    scale: .9
+})
+
+gsap.from(paper, {
+    yPercent: 100,
+    duration: 1.5,
+    onComplete: () => {
+        canOpen = true
+    }
+})
+
+paper.addEventListener('click', () => {
+    if(canOpen) {
+        canOpen = false;
+        openTl.play();
+    }
 });
+
+openTl.to(upperFold, {
+    rotateX: 0
+}).to(lowerFold, {
+    rotateX: 0
+}).to(paper, {
+    scale: 1,
+    rotate: 0,
+    width: "100dvw"
+});
+
+// tl.from(paper, {
+//     yPercent: 100,
+//     scale: .8,
+//     rotate: 20,
+//     onFinish: () => {
+        
+//     }
+// }).from(upperFold, {
+//     rotateX: "179deg" 
+// }).from(lowerFold, {
+//     rotateX: "179deg"
+// });
