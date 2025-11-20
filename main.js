5a174e292348e844bd794d7de822ace0c9374ff4
Pinkify_ <huelleigor@gmail.com>
2025-11-20 21:52:49 +0100

    test

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const decoration = document.querySelector('.decoration');
+const cover = document.querySelector('.cover');
 const paper = document.querySelector('.paper');
 const paperText = document.querySelector('.paper-text');
 const upperFold = document.querySelector('.paper-fold:nth-child(1)');
@@ -5,66 +7,54 @@ const lowerFold = document.querySelector('.paper-fold:nth-child(3)');
 
 gsap.registerPlugin(SplitText);
 
-let canOpen = false
+document.fonts.ready.then(() => {
+    cover.style.display = "none";
 
-let textSplit = SplitText.create(paperText, {
-    type: "words lines",
-    mask: "lines"
+    let canOpen = false
+    let textSplit = SplitText.create(paperText, {
+        type: "words lines",
+        mask: "lines"
+    });
+
+    const openTl = gsap.timeline();
+    openTl.pause();
+
+    gsap.set([upperFold, lowerFold], {
+        rotateX: "179deg"
+    })
+
+    gsap.set(paper, {
+        rotate: 10,
+        scale: .9
+    })
+
+    gsap.from(paper, {
+        yPercent: 100,
+        duration: 1.5,
+        onComplete: () => {
+            canOpen = true
+        }
+    })
+
+    paper.addEventListener('click', () => {
+        if(canOpen) {
+            canOpen = false;
+            openTl.play();
+        }
+    });
+
+    openTl.to(upperFold, {
+        rotateX: 0
+    }).add(() => {
+        decoration.style.display = "none"
+    }, "<25%").to(lowerFold, {
+        rotateX: 0
+    }).to(paper, {
+        scale: 1,
+        rotate: 0,
+        width: "100dvw",
+    }).from(textSplit.words, {
+        yPercent: 100,
+        stagger: .02
+    });
 });
-
-const openTl = gsap.timeline();
-openTl.pause();
-
-gsap.set([upperFold, lowerFold], {
-    rotateX: "179deg"
-})
-
-gsap.set(paper, {
-    rotate: 10,
-    scale: .9
-})
-
-// gsap.set(paperText, {
-//     opacity: 0
-// })
-
-gsap.from(paper, {
-    yPercent: 100,
-    duration: 1.5,
-    onComplete: () => {
-        canOpen = true
-    }
-})
-
-paper.addEventListener('click', () => {
-    if(canOpen) {
-        canOpen = false;
-        openTl.play();
-    }
-});
-
-openTl.to(upperFold, {
-    rotateX: 0
-}).to(lowerFold, {
-    rotateX: 0
-}).to(paper, {
-    scale: 1,
-    rotate: 0,
-    width: "100dvw"
-}).from(textSplit.words, {
-    yPercent: 100,
-    stagger: .02
-});
-
-// tl.from(paper, {
-//     yPercent: 100,
-//     scale: .8,
-//     rotate: 20,
-//     onFinish: () => {
-        
-//     }
-// }).from(upperFold, {
-//     rotateX: "179deg" 
-// }).from(lowerFold, {
-//     rotateX: "179deg"
-// });
