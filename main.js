698bd1bf2d98d1a47e353a1f422d9dfd96c8cc97
Pinkify_ <huelleigor@gmail.com>
2025-11-20 20:00:38 +0100

    a

---
+++ main.js
@@ -0,0 +1,15 @@
+const paper = document.querySelector('.paper');
+const upperFold = document.querySelector('.paper-fold:nth-child(1)');
+const lowerFold = document.querySelector('.paper-fold:nth-child(3)');
+
+gsap.set(paper, {
+    scale: .8
+})
+
+gsap.set(upperFold, {
+    rotateX: "179deg" 
+});
+
+gsap.set(lowerFold, {
+    rotateX: "179deg"
+});
